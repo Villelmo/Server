4f69d96e15b61c65fe16cbf7c9bae200416eb684
DESKTOP-OTAV6MA\admin <William>
2025-10-05 16:31:06 -0600

    Save output in a file

--- turnonServer.ps1
+++ turnonServer.ps1
@@ -1,14 +1,11 @@
 Write-Output "Scanning network..."
 
 # Scan network 
-C:\Users\William\Downloads\Compressed\wakemeonlan-x64\WakeMeOnLan.exe /scan 
+C:\Users\William\Downloads\Compressed\wakemeonlan-x64\WakeMeOnLan.exe /scan /UseIPAddressesRange 1 /IPAddressFrom 192.168.0.1 /IPAddressTo 192.168.0.230 /stext scan.txt
 
 Write-Output "Wake up server...."
 # Turn on the server
-C:\Users\William\Downloads\Compressed\wakemeonlan-x64\WakeMeOnLan.exe /wakeup 192.168.1.4 
-
-# Outputs to the console 
-Write-Host "This is a log message." | Out-File -FilePath "C:\Logs\HostOutput.log" -Append
+C:\Users\William\Downloads\Compressed\wakemeonlan-x64\WakeMeOnLan.exe /wakeup 192.168.0.119
 
 # Window 10, Windows incompatible with command MSG
 #msg DESTKOP-WORK /SERVER:SVRTARSKO "The server ON"
